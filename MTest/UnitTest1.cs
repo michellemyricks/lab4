92f16b3ada98a1dffd63ce5f1a3e3de685a1ca4f
michellemyricks <michellemyricks@gmail.com>
2016-09-21 13:47:52 -0400

    final lab 4

---
+++ MTest/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string a = "3";
+            string b = "2";
+
+            string c = a + b;
+
+            Assert.AreEqual("32", c);
+        }
+        [TestMethod]
+        public void TestMethod_toNUmberTest()
+        {
+            string a = "3";
+            string b = "2";
+
+            int c = int.Parse(a) + int.Parse(b);
+                
+                
+
+            Assert.AreEqual(5, c);
+        }
+    }
+}
--- lab4/Program.cs
+++ lab4/Program.cs
@@ -8,8 +8,32 @@ namespace lab4
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-        }
+            Console.WriteLine("Welcome to the Factorial Calculator!\n");
+            Console.WriteLine("Enter an integer that's greater than 0 but less than 10:\n");
+            string numberToCountDownFrom = Console.ReadLine();
+
+
+            int value = Convert.ToInt32(numberToCountDownFrom);
+
+
+            if (value > 0 || value < 10) {
+
+
+
+                for (value  = 0; value < 5; i++)
+                {
+                    Console.WriteLine(i);
+
+
+                }
+
+
+
+
+
+                //for (int i = 0; i < 10; i++) ;
+            }
     }
 }
